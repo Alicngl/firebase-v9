044333c6db4b0280ed74a2f34b8a1694af853a2b
Ali Cangül <97682611+Alicngl@users.noreply.github.com>
2022-05-13 13:35:29 +0300

    proje yüklendi

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import {initializeApp} from "firebase/app"
+import {getFirestore} from "firebase/firestore"
+import {getAuth} from "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBPzdrxfZoISNQeZrHYGtR0dz6yg_xyMvo",
+    authDomain: "modern-react-app-9bf56.firebaseapp.com",
+    projectId: "modern-react-app-9bf56",
+    storageBucket: "modern-react-app-9bf56.appspot.com",
+    messagingSenderId: "1015475790763",
+    appId: "1:1015475790763:web:c0924b7420e586f299e8ee"
+};
+
+initializeApp(firebaseConfig)
+
+const db=getFirestore()
+
+const auth=getAuth()
+
+export {db,auth}
